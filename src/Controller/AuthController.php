e739beadf934e2b61eb21cc48dc453b3adf753d9
davidgm26 <davidgama260402@gmail.com>
2025-02-03 20:48:27 +0100

    implementacion de login y registro

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -2,14 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\Usuario;
 use App\Service\UsuarioService;
-use Doctrine\ORM\EntityManagerInterface;
-use Rol;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api/auth')]
@@ -21,16 +17,11 @@ final class AuthController extends AbstractController
     {}
 
     #[Route('/registro', name: 'registrar_usuario_cliente', methods: ['POST'])]
-    public function register(Request $request,
-                             UserPasswordHasherInterface $passwordHasher,
-                             EntityManagerInterface $entityManager): JsonResponse
+    public function register(Request $request): JsonResponse
     {
         $data = json_decode($request->getContent(), true);
 
         $this->usuarioService->registrarUsuario($data);
-
-
-
 
         return new JsonResponse(['message' => 'Usuario registrado con éxito'], 201);
     }
